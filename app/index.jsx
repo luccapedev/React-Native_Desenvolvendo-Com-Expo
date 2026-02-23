71e0c95e0d7cdaa4f33bd5d258214c132e0a8150
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-02-23 17:27:47 -0300

    Aplicando estilos no projeto com StyleSheet

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.jsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Edit app/index.jsx to edit this screen!!!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#021123",
+  },
+  text: {
+    color: "#FFF",
+  },
+});
