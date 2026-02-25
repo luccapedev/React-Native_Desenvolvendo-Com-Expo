383a4f6c6c552b75700bf4ea2d1d5b1b944fd70d
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-02-25 10:25:55 -0300

    Preparando o timer

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import { ActionButton } from "../components/ActionButton";
 import { FokusButton } from "../components/FokusButton";
@@ -28,7 +28,21 @@ const pomodoro = [
 
 export default function Index() {
 
-  const [timerType, setTimerType] = useState(pomodoro[1])
+  const [timerType, setTimerType] = useState(pomodoro[0])
+
+  const timerRef = useRef(null)
+
+  const toggleTimer = () => {
+    if (timerRef.current) {
+      // pausar
+      clearInterval(timerRef.current)
+      return
+    }
+    const id = setInterval(() => {
+      console.log('timer rolando')
+    }, 1000 )
+    timerRef.current = id
+  }
 
   return (
     <View 
@@ -47,7 +61,10 @@ export default function Index() {
           ))}
         </View>
         <Timer totalSeconds={timerType.initialValue}/>
-        <FokusButton/>
+        <FokusButton 
+          title={timerRef.current ? 'Pausar' : 'Começar'}
+          onPress={toggleTimer}
+        />
       </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>
